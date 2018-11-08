cc0ca2d7660f500b5bb89874173b039bd884a03b
lealcalhau <calhau.bruno88@gmail.com>
2018-11-08 13:56:12 -0200

    Adicionando Categorias Model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,16 @@
 import { InMemoryDbService } from "angular-in-memory-web-api"
 
+import { Category } from "./pages/categories/shared/category.model"
+
 export class InMemoryDatabase implements InMemoryDbService {
     createDb(){
-        const categories = [
+        const categories: Category[] = [
             { id: 1, name: "Moradia", description: "Pagaementos de Contas da Casa"},
             { id: 2, name: "Saúde", description: "Plano de Saúde e Remédios"},
             { id: 3, name: "Lazer", description: "Cinema, parques, prais, etc"},
             { id: 4, name: "Salário", description: "Recebimento de Salário"},
             { id: 5, name: "Freelas", description: "Trabalhos como freelancer"}
+            
         ];
         return { categories }
     }
