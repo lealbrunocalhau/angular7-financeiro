c0c814707b841e1ddccf6130fa980df2e507a5d8
lealcalhau <calhau.bruno88@gmail.com>
2018-11-13 21:44:44 -0200

    Associacao de categoria com lançamento feito para adaptar no caso do web-api

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -3,8 +3,13 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { Observable, throwError } from "rxjs";
 import { map, catchError, flatMap } from "rxjs/operators";
+
+import { CategoryService } from "../../categories/shared/category.service";
+
 import { Entry } from "./entry.model";
 import { element } from 'protractor';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +17,8 @@ import { element } from 'protractor';
 export class EntryService {
 
   private apiPath: string = "api/entries"
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient,
+                private categoryService: CategoryService) { }
 
   getAll(): Observable<Entry[]>{
     console.log('entri denovo')
@@ -31,21 +37,54 @@ export class EntryService {
     )
   }
 
+  // create(entry: Entry): Observable<Entry>{
+  //   return this.http.post(this.apiPath, entry).pipe(
+  //     catchError(this.handleError),
+  //     map(this.jsonDataToEntry)
+  //   )
+  // }
+
   create(entry: Entry): Observable<Entry>{
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
+
+    //adaptacao para criar category dentro do objeto Entry
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        )
+      })
     )
   }
 
+  // update(entry: Entry): Observable<Entry>{
+  //   //montar url q vou montar
+  //   const url = `${this.apiPath}/${entry.id}`;
+  //   return this.http.put(url, entry).pipe(
+  //     catchError(this.handleError),
+  //     map(()=> entry)
+  //   )
+  // }
+
+  //Adaptacao
   update(entry: Entry): Observable<Entry>{
     //montar url q vou montar
     const url = `${this.apiPath}/${entry.id}`;
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(()=> entry)
+
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category
+        
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(()=> entry)
+        )
+      })
     )
+    
   }
+
 
   delete(id: number): Observable<any> {
     const url = `${this.apiPath}/${id}`;
